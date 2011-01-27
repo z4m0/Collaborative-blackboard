1f4d10fa34a4b43e5b8bd5462d52deddfe4b6d6a
z4m0 <martizzcc@gmail.com>
2011-01-27 17:12:17 +0100

    some

--- datamanager/constants.php
+++ datamanager/constants.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_HOST', "localhost");
 define('DB_USER', "root");
-define('DB_PASSWORD', "goma2");
+define('DB_PASSWORD', "pass");
 define('DB_NAME', "snipets");
 define('DB_PREFIX', "");
 
